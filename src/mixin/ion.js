ca00a4e13a9aae63a55852f53d3afe00b80dba9d
mtsukuda <mats.tsukuda@gmail.com>
2021-05-06 11:59:17 +0900

    create mixin ion js.

---
+++ src/mixin/ion.js
@@ -0,0 +1,25 @@
+export default {
+  data() {
+    return {
+      ionTags: [
+        { tag: "div", label: "div" },
+        { tag: "p", label: "p" },
+        { tag: "IonContent", label: "IonContent" },
+        { tag: "IonCard", label: "IonCard" },
+        { tag: "IonCardHeader", label: "IonCardHeader" },
+        { tag: "IonCardTitle", label: "IonCardTitle", noCR: "yes" },
+        { tag: "IonCardSubtitle", label: "IonCardSubtitle", noCR: "yes" },
+        { tag: "IonCardContent", label: "IonCardContent" },
+        { tag: "IonItem", label: "IonItem" },
+        { tag: "IonIcon", label: "IonIcon" },
+        { tag: "IonLabel", label: "IonLabel" },
+        { tag: "IonButton", label: "IonButton" },
+      ],
+    };
+  },
+  methods: {
+    something: function () {
+      console.log("do something.");
+    },
+  },
+};
