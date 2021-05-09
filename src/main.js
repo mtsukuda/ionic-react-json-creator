f309f5c78d23a6586f8f11c648800ca430610d92
mtsukuda <mats.tsukuda@gmail.com>
2021-05-09 22:29:27 +0900

    add import: BootstrapVue.

--- src/main.js
+++ src/main.js
@@ -2,10 +2,11 @@ Vue.config.devtools = true;
 
 import Vue from "vue";
 import App from "./App.vue";
-import { BootstrapVue } from "bootstrap-vue";
+import { BootstrapVue, BootstrapVueIcons } from "bootstrap-vue";
 import VModal from "vue-js-modal";
 
 Vue.use(BootstrapVue);
+Vue.use(BootstrapVueIcons);
 Vue.use(VModal);
 
 Vue.config.productionTip = false;
