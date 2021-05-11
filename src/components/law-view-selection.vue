7c7fc94c0d30affad80c80f1c702d8906bfc2c60
mtsukuda <mats.tsukuda@gmail.com>
2021-05-11 11:16:45 +0900

    arrengement: checkbox class and test.

--- src/components/law-view-selection.vue
+++ src/components/law-view-selection.vue
@@ -1,17 +1,32 @@
 <template>
   <div class="mt-2">
     <form>
-      <input type="checkbox" id="tags" value="Jack" v-model="value.tags" />
+      <input type="checkbox" id="tags" value="tags" v-model="value.tags" />
       <label for="tags" class="p-1">tags</label>
       <input
         type="checkbox"
         id="imports"
         value="Jack"
         v-model="value.imports"
+        class="ml-2"
       />
       <label for="imports" class="p-1">Imports</label>
-      <input type="checkbox" id="fetch" value="Jack" v-model="value.fetch" />
+      <input
+        type="checkbox"
+        id="fetch"
+        value="fetch"
+        v-model="value.fetch"
+        class="ml-2"
+      />
       <label for="fetch" class="p-1">fetch</label>
+      <input
+        type="checkbox"
+        id="debug"
+        value="debug"
+        v-model="value.fetch"
+        class="ml-2"
+      />
+      <label for="debug" class="p-1 text-danger">debug</label>
     </form>
   </div>
 </template>
