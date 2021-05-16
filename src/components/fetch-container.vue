258f16e023eaa5e48c18f22432f3f5fd467a4657
mtsukuda <mats.tsukuda@gmail.com>
2021-05-16 14:13:50 +0900

    implement fetch create modal show up.

--- src/components/fetch-container.vue
+++ src/components/fetch-container.vue
@@ -2,15 +2,33 @@
   <div class="container pl-1 pr-1">
     <div class="border rounded fetch-container-div mb-1">
       <div class="h5 m-2 pl-1 mb-1">fetch</div>
+      <div>
+        <button
+          v-on:click="showFetchCreateModal"
+          class="btn btn-outline-info property-btn btn-sm m-1"
+        >
+          新規作成
+        </button>
+        <fetch-create-modal></fetch-create-modal>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import fetchCreateModal from "./fetch-create-modal";
 export default {
   name: "fetch-container",
+  components: {
+    fetchCreateModal,
+  },
   props: {
     value: {},
+  },
+  methods: {
+    showFetchCreateModal: function () {
+      this.$modal.show("fetch-create-modal");
+    },
   },
 };
 </script>
---
+++ src/components/fetch-create-modal.vue
@@ -0,0 +1,32 @@
+<template>
+  <modal name="fetch-create-modal" :draggable="true" :resizable="true">
+    <div class="modal-header">
+      <h2>fetch create</h2>
+    </div>
+    <div class="modal-body">
+      <div>Fetch Create Modal💪</div>
+      <div class="mt-3 mx-auto text-center">
+        <button
+          v-on:click="hide"
+          class="btn btn-outline-secondary property-btn btn-sm m-1"
+        >
+          閉じる
+        </button>
+      </div>
+    </div>
+  </modal>
+</template>
+
+<script>
+export default {
+  name: "fetch-create-modal",
+  methods: {
+    show: function () {},
+    hide: function () {
+      this.$modal.hide("fetch-create-modal");
+    },
+  },
+};
+</script>
+
+<style scoped></style>
