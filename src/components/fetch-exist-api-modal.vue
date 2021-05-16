f1e696c9cd65ced1507a49c6d58a3507dc668030
mtsukuda <mats.tsukuda@gmail.com>
2021-05-16 17:29:14 +0900

    create component: fetch-exist-api-modal.

---
+++ src/components/fetch-exist-api-modal.vue
@@ -0,0 +1,32 @@
+<template>
+  <modal name="fetch-exist-api-modal" :draggable="true" :resizable="true">
+    <div class="modal-header">
+      <h2>Exist API</h2>
+    </div>
+    <div class="modal-body">
+      <div>Exist API Modal💪</div>
+    </div>
+    <div class="modal-footer">
+      <button
+        v-on:click="hide"
+        class="btn btn-outline-secondary property-btn btn-sm m-1"
+      >
+        閉じる
+      </button>
+    </div>
+  </modal>
+</template>
+
+<script>
+export default {
+  name: "fetch-exist-api-modal",
+  methods: {
+    show: function () {},
+    hide: function () {
+      this.$modal.hide("fetch-exist-api-modal");
+    },
+  },
+};
+</script>
+
+<style scoped></style>
