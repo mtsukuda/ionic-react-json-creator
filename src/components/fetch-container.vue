c5c1cd85cbd9cf2220a6dfa307cc7862a89c8ba7
mtsukuda <mats.tsukuda@gmail.com>
2021-05-22 00:02:18 +0900

    implement: show fetch edit modal.

--- src/components/fetch-container.vue
+++ src/components/fetch-container.vue
@@ -4,7 +4,7 @@
       <div class="h5 m-2 pl-1 mb-1">fetch</div>
       <b-container class="bv-example-row">
         <b-row
-          v-for="fetch in value"
+          v-for="(fetch, index) in value"
           class="border rounded m-1 p-2 fetch-api-div"
         >
           <b-col class="p-0">
@@ -14,7 +14,12 @@
             </div>
           </b-col>
           <b-col cols="3" class="text-right p-0 pt-1">
-            <b-button size="sm" variant="outline-primary" class="mr-1">
+            <b-button
+              size="sm"
+              variant="outline-primary"
+              class="mr-1"
+              v-on:click="showFetchEditModal(fetch.method, index)"
+            >
               <b-icon icon="pencil" aria-label="Edit"></b-icon>
             </b-button>
             <b-button size="sm" variant="outline-danger">
@@ -31,6 +36,7 @@
           新規作成
         </button>
         <fetch-create-modal v-model="value"></fetch-create-modal>
+        <fetch-edit-get-modal v-model="value"></fetch-edit-get-modal>
       </div>
     </div>
   </div>
@@ -38,10 +44,12 @@
 
 <script>
 import fetchCreateModal from "./fetch-create-modal";
+import fetchEditGetModal from "./fetch-edit-get-modal";
 export default {
   name: "fetch-container",
   components: {
     fetchCreateModal,
+    fetchEditGetModal,
   },
   props: {
     value: {},
@@ -49,6 +57,10 @@ export default {
   methods: {
     showFetchCreateModal: function () {
       this.$modal.show("fetch-create-modal");
+    },
+    showFetchEditModal: function (method, index) {
+      console.log(`fetch-edit-${method}-modal`);
+      this.$modal.show(`fetch-edit-${method}-modal`, { index: index });
     },
   },
 };
---
+++ src/components/fetch-edit-get-modal.vue
@@ -0,0 +1,156 @@
+<template>
+  <modal
+    name="fetch-edit-get-modal"
+    @before-open="beforeOpen"
+    :draggable="true"
+    :resizable="true"
+    :scrollable="true"
+    height="auto"
+  >
+    <div class="modal-body">
+      <div>
+        <div>Fetch method type</div>
+        <div class="m-2 font-weight-bold">GET</div>
+        <label for="inputFetchName">Fetch function name</label>
+        <div class="col-sm input-sm">
+          <input
+            class="form-control"
+            id="inputFetchName"
+            type="text"
+            placeholder="Fetch Name"
+            v-model="input.name"
+          />
+        </div>
+      </div>
+      <div class="mt-1 mb-1">APIs</div>
+      <div class="border rounded fetch-apis-div mt-1 ml-3 mr-3">
+        <div class="small mb-2">
+          <b-nav-item-dropdown text="Add API [+]" class="list-unstyled pt-2">
+            <b-dropdown-item
+              v-for="api in apiSelect"
+              v-on:click="addApi(api.type)"
+              class="dropdown-mine small"
+              >{{ api.label }}</b-dropdown-item
+            >
+          </b-nav-item-dropdown>
+        </div>
+        <div>
+          <b-container class="p-0">
+            <b-row
+              v-for="api in apis"
+              class="border rounded m-2 p-2 fetch-api-div"
+            >
+              <b-col class="p-0">
+                <div class="small">
+                  {{ api.responseTypeName }}<br />
+                  {{ api.uri }}
+                </div>
+              </b-col>
+              <b-col cols="3" class="p-0 pt-1 text-right">
+                <b-button size="sm" variant="outline-primary" class="mr-1">
+                  <b-icon icon="pencil" aria-label="Edit"></b-icon>
+                </b-button>
+                <b-button size="sm" variant="outline-danger">
+                  <b-icon icon="trash" aria-label="Delete"></b-icon>
+                </b-button>
+              </b-col>
+            </b-row>
+          </b-container>
+        </div>
+        <fetch-create-get-api-external-modal
+          v-model="apis"
+          @close="closeModal"
+          @commit="commitModal"
+        ></fetch-create-get-api-external-modal>
+      </div>
+    </div>
+    <div class="modal-footer">
+      <button
+        v-on:click="hide"
+        class="btn btn-outline-secondary property-btn btn-sm m-1"
+      >
+        閉じる
+      </button>
+      <button
+        v-on:click="commit"
+        class="btn btn-outline-primary property-btn btn-sm m-1"
+        :disabled="createDisable"
+      >
+        作成
+      </button>
+    </div>
+  </modal>
+</template>
+
+<script>
+import FetchCreateGetApiExternalModal from "./fetch-create-get-api-external-modal";
+export default {
+  name: "fetch-edit-get-modal",
+  components: { FetchCreateGetApiExternalModal },
+  props: {
+    value: {},
+  },
+  data() {
+    return {
+      input: {
+        name: "",
+        method: "",
+      },
+      apiSelect: [
+        { type: "external", label: "External API" },
+        { type: "internal", label: "Internal API" },
+      ],
+      apis: [],
+    };
+  },
+  mounted() {
+    // this.input.name = this.value[this.index].name;
+    // this.apis = this.value[this.index].apis;
+  },
+  computed: {
+    createDisable() {
+      return !this.apis.length || !this.input.name;
+    },
+  },
+  methods: {
+    beforeOpen(event) {
+      let index = event.params.index;
+      console.log(event.params.index);
+      this.input.name = this.value[index].name;
+      this.apis = this.value[index].apis;
+    },
+    hide: function () {
+      this.$modal.hide("fetch-edit-get-modal");
+    },
+    addApi: function (apiType) {
+      if (apiType === "external") {
+        this.$modal.show("fetch-create-get-api-external-modal");
+      }
+      console.log(apiType);
+    },
+    closeModal: function () {
+      console.log("close");
+    },
+    commitModal: function () {
+      console.log("commit");
+    },
+    commit: function () {
+      this.value.push({
+        method: "get",
+        name: this.input.name,
+        apis: this.apis,
+      });
+      this.hide();
+    },
+  },
+};
+</script>
+
+<style scoped>
+.fetch-apis-div {
+  border-color: grey;
+}
+.fetch-api-div {
+  color: #6f6f6f;
+}
+</style>
