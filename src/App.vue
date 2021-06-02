aa2293b928738052cb94293e3a13081b867407b3
mtsukuda <mats.tsukuda@gmail.com>
2021-06-02 15:55:16 +0900

    delete element lifeCycleMethodsSeed from seed JSON.

--- src/App.vue
+++ src/App.vue
@@ -63,7 +63,6 @@ export default {
         name: "",
         import: [],
         lifeCycleMethods: [],
-        lifeCycleMethodsSeed: [],
         fetch: [],
         renderBeforeReturn: [],
         defaultProps: [],
