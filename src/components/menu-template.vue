82790e23d35e4773e6e5ed077b85b8ba71797d32
mtsukuda <mats.tsukuda@gmail.com>
2021-06-20 09:45:50 +0900

    add json-view-menu-raw-json. add data().

--- src/components/menu-template.vue
+++ src/components/menu-template.vue
@@ -1,13 +1,75 @@
 <template>
   <div class="row">
     <div class="col-6">menu layout area</div>
-    <div class="col-6">menu json view rawjson</div>
+    <div class="col-6">
+      <json-view-menu-raw-json v-model="menu" />
+    </div>
   </div>
 </template>
 
 <script>
+import JsonViewMenuRawJson from "./menu/json-view-menu-raw-json";
 export default {
   name: "menu-template",
+  components: { JsonViewMenuRawJson },
+  props: {
+    value: {},
+  },
+  data() {
+    return {
+      menu: [
+        {
+          strTitle: "ダッシュボード",
+          strUrl: "/page/Dashboard",
+          icon: "extensionPuzzle",
+          component: "Default",
+          redirect: "yes",
+        },
+        {
+          strTitle: "BASE アプリ",
+          strUrl: "/page/Base",
+          icon: "storefront",
+          component: "Default",
+        },
+        {
+          strTitle: "その他",
+          strUrl: "/page/Others",
+          icon: "layers",
+          component: "Default",
+        },
+        {
+          strTitle: "Sample",
+          strUrl: "/sample",
+          icon: "planet",
+          component: "SamplePage",
+        },
+        {
+          strTitle: "Sample2",
+          strUrl: "/sample2",
+          icon: "basketball",
+          component: "SamplePage2",
+        },
+        {
+          strTitle: "MyCard",
+          strUrl: "/mycard",
+          icon: "basketball",
+          component: "MyCardComponentPage",
+        },
+        {
+          strTitle: "MyOmanko",
+          strUrl: "/omanko",
+          icon: "basketball",
+          component: "OmankoComponentPage",
+        },
+        {
+          strTitle: "MyTakosuke",
+          strUrl: "/takosuke",
+          icon: "basketball",
+          component: "TakosukeComponentPage",
+        },
+      ],
+    };
+  },
 };
 </script>
 
