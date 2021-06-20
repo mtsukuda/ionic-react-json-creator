88561612e3e1cda4ff746347221104be3890116e
mtsukuda <mats.tsukuda@gmail.com>
2021-06-20 17:30:18 +0900

    create menu container/

---
+++ src/components/menu/menu-container.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container pl-1 pr-1">
+    <div v-for="menu in value">
+      {{ menu.strTitle }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "menu-container",
+  props: {
+    value: {},
+    configJson: {},
+  },
+};
+</script>
+
+<style scoped></style>
