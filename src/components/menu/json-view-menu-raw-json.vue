efd7b99ced9efdb896d36160342892a106a7100c
mtsukuda <mats.tsukuda@gmail.com>
2021-06-20 09:45:06 +0900

    create json view menu raw json.

---
+++ src/components/menu/json-view-menu-raw-json.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="bg-light p-3 mt-1 small">
+    <pre>{{ raw }}</pre>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "json-view-menu-raw-json",
+  props: {
+    value: {},
+  },
+  computed: {
+    raw() {
+      return JSON.stringify(this.value, null, 2);
+    },
+  },
+};
+</script>
+
+<style scoped></style>
