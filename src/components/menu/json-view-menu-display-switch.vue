def03a72534cd2a35edc574cb5aab99c1747e3b1
mtsukuda <mats.tsukuda@gmail.com>
2021-06-23 10:27:49 +0900

    create json-view-menu-display-switch.

---
+++ src/components/menu/json-view-menu-display-switch.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="mt-2">
+    <form>
+      <input
+        type="checkbox"
+        id="debug"
+        value="debug"
+        v-model="value.debug"
+        class="ml-2"
+      />
+      <label for="debug" class="p-1 text-danger">debug</label>
+      <input
+        type="checkbox"
+        id="tag-uid"
+        value="tagUid"
+        v-model="value.menuUid"
+        class="ml-2"
+      />
+      <label for="tag-uid" class="p-1 text-danger">menu uid</label>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "json-view-display-switch",
+  props: {
+    value: {},
+  },
+};
+</script>
+
+<style scoped></style>
