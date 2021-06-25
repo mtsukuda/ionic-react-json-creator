4b289dfd82fddb70e9717d4af21c6270485c284d
mtsukuda <mats.tsukuda@gmail.com>
2021-06-26 07:17:07 +0900

    add menu-header.

---
+++ src/components/menu/menu-header.vue
@@ -0,0 +1,27 @@
+<template>
+  <form>
+    <div class="form-group mt-2">
+      <label for="inputMenuHeaderTitle">Menu Header Title</label>
+      <div class="col-sm">
+        <input
+          class="form-control"
+          id="inputMenuHeaderTitle"
+          type="text"
+          placeholder="MyComponent"
+          v-model="value.header.strTitle"
+        />
+      </div>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "menu-header",
+  props: {
+    value: {},
+  },
+};
+</script>
+
+<style scoped></style>
