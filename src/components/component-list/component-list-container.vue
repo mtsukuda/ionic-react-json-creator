5185de661379fd32ecc01e86a96b696be79b3119
mtsukuda <mats.tsukuda@gmail.com>
2021-06-26 22:27:49 +0900

    create component-list.

---
+++ src/components/component-list/component-list-container.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container pl-1 pr-1">
+    <div v-for="component in value">{{ component.name }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "component-list-container",
+  props: {
+    value: {},
+  },
+  methods: {},
+};
+</script>
+
+<style scoped></style>
