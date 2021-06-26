a6d228cfa99fb4e6389acf1cc0ff643b5e904aaf
mtsukuda <mats.tsukuda@gmail.com>
2021-06-26 22:28:16 +0900

    enhance component-list-template.

--- src/components/component-list-template.vue
+++ src/components/component-list-template.vue
@@ -1,8 +1,21 @@
-<template></template>
+<template>
+  <div>
+    <div class="row">
+      <div class="col-6">
+        <div class="mt-3">
+          <component-list-container v-model="value" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
 
 <script>
+import ComponentListContainer from "./component-list/component-list-container";
+
 export default {
   name: "component-list-template",
+  components: { ComponentListContainer },
   props: {
     value: {},
   },
