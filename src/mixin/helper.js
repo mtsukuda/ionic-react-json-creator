6aa2e8ef4fa1d23df0ee519b2eee38ee5d58fa09
mtsukuda <mats.tsukuda@gmail.com>
2021-06-26 22:18:36 +0900

    refactor method: suggestionStr() --> add capitalize as an argument.

--- src/mixin/helper.js
+++ src/mixin/helper.js
@@ -1,11 +1,12 @@
 export default {
   methods: {
-    suggestionStr: function (length = 8) {
+    suggestionStr: function (capitalize = false, length = 8) {
       let S = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
       let N = length;
-      return Array.from(Array(length))
+      let result = Array.from(Array(N))
         .map(() => S[Math.floor(Math.random() * S.length)])
         .join("");
+      return capitalize ? result[0].toUpperCase() + result.slice(1) : result;
     },
   },
 };
