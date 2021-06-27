77ca5ba1370de6449f051eeac7932abc2366b0d8
mtsukuda <mats.tsukuda@gmail.com>
2021-06-27 16:39:57 +0900

    implement method: uID().

--- src/mixin/helper.js
+++ src/mixin/helper.js
@@ -8,5 +8,12 @@ export default {
         .join("");
       return capitalize ? result[0].toUpperCase() + result.slice(1) : result;
     },
+    uID: function (radix = 16) {
+      let strong = 1000;
+      return (
+        new Date().getTime().toString(radix) +
+        Math.floor(strong * Math.random()).toString(radix)
+      );
+    },
   },
 };
