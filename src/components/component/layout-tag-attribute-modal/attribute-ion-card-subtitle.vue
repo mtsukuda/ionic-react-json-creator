bb9f72a4f53c1a7402521cf42ef7b6789fd95dd9
mtsukuda <mats.tsukuda@gmail.com>
2021-07-05 13:27:46 +0900

    create attribute-ion-card-subtitle.vue.

---
+++ src/components/component/layout-tag-attribute-modal/attribute-ion-card-subtitle.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <label for="inputIonCardSubTitle" class="small"
+    >Sub Title<b-form-checkbox
+        id="use-fetch-code"
+        name="use-fetch-code"
+        value="yes"
+        unchecked-value=""
+        v-model="value.code"
+    >
+      Use Fetch Code
+    </b-form-checkbox></label
+    >
+    <div class="col-sm">
+      <input
+          class="form-control"
+          size="sm"
+          id="inputIonCardSubTitle"
+          type="text"
+          placeholder="Sub Title"
+          v-model="value.content"
+          list="list-response"
+      />
+    </div>
+    <datalist id="list-response">
+      <option v-for="list in fetchTemp.responseList">{{ list }}</option>
+    </datalist>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "attribute-ion-card-subtitle",
+  props: {
+    value: {},
+    fetchTemp: {}
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
