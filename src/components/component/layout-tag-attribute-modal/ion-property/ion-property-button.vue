3a8f4d135d44ceae6a1ca723177b32f0adea4d82
mtsukuda <mats.tsukuda@gmail.com>
2021-07-06 17:57:04 +0900

    create ion-property-button.

---
+++ src/components/component/layout-tag-attribute-modal/ion-property/ion-property-button.vue
@@ -0,0 +1,29 @@
+<template>
+<div>
+  <b-form-group
+      label-cols-sm="2"
+      label-cols-lg="2"
+      content-cols-sm
+      content-cols-lg="8"
+      description="If true, a button tag will be rendered and the card will be tappable."
+      label="button"
+      label-for="input-horizontal"
+  >
+    <b-form-select v-model="value.readyProps.button" :options="[{ value: true, text: 'true' },{ value: false, text: 'false' },]"></b-form-select>
+  </b-form-group>
+
+</div>
+</template>
+
+<script>
+export default {
+  name: "ion-property-button",
+  props: {
+    value: {},
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
