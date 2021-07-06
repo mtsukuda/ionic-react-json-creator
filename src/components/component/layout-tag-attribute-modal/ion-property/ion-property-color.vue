c6bee913a6122594b3606bf059cd593f9aac7aa7
mtsukuda <mats.tsukuda@gmail.com>
2021-07-06 18:07:50 +0900

    create ion-property-color.

---
+++ src/components/component/layout-tag-attribute-modal/ion-property/ion-property-color.vue
@@ -0,0 +1,38 @@
+<template>
+<div>
+  <b-form-group
+      label-cols-sm="2"
+      label-cols-lg="2"
+      content-cols-sm
+      content-cols-lg="8"
+      description="The color to use from your application's color palette."
+      label="color"
+      label-for="input-horizontal"
+  >
+    <b-form-select v-model="value.readyProps.color" :options="options" />
+  </b-form-group>
+
+</div>
+</template>
+
+<script>
+export default {
+  name: "ion-property-color",
+  props: {
+    value: {},
+  },
+  data() {
+    return {
+      options: [
+        {value: '', text: 'Please select color.'},
+        {value: 'primary', text: 'primary'},
+        {value: 'secondary', text: 'secondary'}
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
