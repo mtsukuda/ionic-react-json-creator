330411a97ec59634c57aad39ec77975505fb29c7
mtsukuda <mats.tsukuda@gmail.com>
2021-07-06 16:47:27 +0900

    create attribute-ion-card.

---
+++ src/components/component/layout-tag-attribute-modal/attribute-ion-card.vue
@@ -0,0 +1,28 @@
+<template>
+  <div v-if="tagTemp.editTag === 'IonCard'">
+    <b-form-checkbox
+        id="use-fetch-code"
+        name="use-fetch-code"
+        value="yes"
+        unchecked-value=""
+        v-model="value.code"
+    >
+      button
+    </b-form-checkbox>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "attribute-ion-card",
+  props: {
+    value: {},
+    tagTemp: {},
+    fetchTemp: {}
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
