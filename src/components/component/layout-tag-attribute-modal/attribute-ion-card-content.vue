8ee32842fd156b0eec41edc32433fd24895ff23c
mtsukuda <mats.tsukuda@gmail.com>
2021-07-06 14:50:06 +0900

    create attribute-ion-card-content.

---
+++ src/components/component/layout-tag-attribute-modal/attribute-ion-card-content.vue
@@ -0,0 +1,44 @@
+<template>
+  <div v-if="tagTemp.editTag === 'IonCardContent'">
+    <label for="inputIonCardContent"
+    >コンテンツ<b-form-checkbox
+        id="use-fetch-code"
+        name="use-fetch-code"
+        value="yes"
+        unchecked-value=""
+        v-model="value.code"
+    >
+      Use Fetch Code
+    </b-form-checkbox></label
+    >
+    <div class="col-sm">
+      <input
+          class="form-control"
+          size="sm"
+          id="inputIonCardContent"
+          type="text"
+          placeholder="コンテンツ：改行させたい場合は<br />で改行"
+          v-model="value.content"
+          list="list-response"
+      />
+    </div>
+    <datalist id="list-response">
+      <option v-for="list in fetchTemp.responseList">{{ list }}</option>
+    </datalist>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "attribute-ion-card-content",
+  props: {
+    value: {},
+    tagTemp: {},
+    fetchTemp: {}
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
