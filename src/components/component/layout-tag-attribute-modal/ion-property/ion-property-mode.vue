b00d9cb36005599a89b6aee59b96f06eff001984
mtsukuda <mats.tsukuda@gmail.com>
2021-07-06 19:19:35 +0900

    create ion-property-mode.

---
+++ src/components/component/layout-tag-attribute-modal/ion-property/ion-property-mode.vue
@@ -0,0 +1,38 @@
+<template>
+<div>
+  <b-form-group
+      label-cols-sm="2"
+      label-cols-lg="2"
+      content-cols-sm
+      content-cols-lg="8"
+      description="The mode determines which platform styles to use."
+      label="color"
+      label-for="input-horizontal"
+  >
+    <b-form-select v-model="value.readyProps.mode" :options="options" />
+  </b-form-group>
+
+</div>
+</template>
+
+<script>
+export default {
+  name: "ion-property-mode",
+  props: {
+    value: {},
+  },
+  data() {
+    return {
+      options: [
+        {value: '', text: 'Please select mode.'},
+        {value: "'ios'", text: 'ios'},
+        {value: "'md'", text: 'md'},
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
