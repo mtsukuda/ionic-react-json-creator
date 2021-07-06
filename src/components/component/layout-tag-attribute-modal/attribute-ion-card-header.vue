f4de2b260b0d9bb2caf01f220b5c6eab9ec84aba
mtsukuda <mats.tsukuda@gmail.com>
2021-07-06 19:26:04 +0900

    create attribute-ion-card-header.

---
+++ src/components/component/layout-tag-attribute-modal/attribute-ion-card-header.vue
@@ -0,0 +1,24 @@
+<template>
+  <div v-if="tagTemp.editTag === 'IonCardHeader'">
+    <ion-property-color v-model="value" />
+    <ion-property-mode v-model="value" />
+  </div>
+</template>
+
+<script>
+import IonPropertyColor from "./ion-property/ion-property-color";
+import IonPropertyMode from "./ion-property/ion-property-mode";
+export default {
+  name: "attribute-ion-card-header",
+  components: {IonPropertyColor, IonPropertyMode},
+  props: {
+    value: {},
+    tagTemp: {},
+    fetchTemp: {}
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
