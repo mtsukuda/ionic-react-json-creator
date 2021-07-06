ab16f2098beccfbefbd8b7bdbe21dd99ddf14da3
mtsukuda <mats.tsukuda@gmail.com>
2021-07-06 18:08:16 +0900

    git add ion-property-color.

--- src/components/component/layout-tag-attribute-modal/attribute-ion-card.vue
+++ src/components/component/layout-tag-attribute-modal/attribute-ion-card.vue
@@ -1,14 +1,16 @@
 <template>
   <div v-if="tagTemp.editTag === 'IonCard'">
     <ion-property-button v-model="value" />
+    <ion-property-color v-model="value" />
   </div>
 </template>
 
 <script>
 import IonPropertyButton from "./ion-property/ion-property-button";
+import IonPropertyColor from "./ion-property/ion-property-color";
 export default {
   name: "attribute-ion-card",
-  components: {IonPropertyButton},
+  components: {IonPropertyColor, IonPropertyButton},
   props: {
     value: {},
     tagTemp: {},
