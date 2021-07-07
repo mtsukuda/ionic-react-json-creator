10dc773ac7801fd743b3f1ab721e5521af1f192c
mtsukuda <mats.tsukuda@gmail.com>
2021-07-07 14:30:31 +0900

    create component: ion-property-disable.

---
+++ src/components/component/layout-tag-attribute-modal/ion-property/ion-property-disable.vue
@@ -0,0 +1,38 @@
+<template>
+<div>
+  <b-form-group
+      label-cols-sm="2"
+      label-cols-lg="2"
+      content-cols-sm
+      content-cols-lg="8"
+      description="If true, the user cannot interact with the component."
+      label="Button"
+      label-for="input-horizontal"
+  >
+    <b-form-select v-model="value.readyProps.disable" :options="options" />
+  </b-form-group>
+
+</div>
+</template>
+
+<script>
+export default {
+  name: "ion-property-disable",
+  props: {
+    value: {},
+  },
+  data() {
+    return {
+      options: [
+        {value: '', text: 'Please select true/false.'},
+        {value: "{true}", text: 'true'},
+        {value: "{false}", text: 'false'}
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
