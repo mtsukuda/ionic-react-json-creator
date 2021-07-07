60483e2d3d66618dd01a25bb86c42a4055f80242
mtsukuda <mats.tsukuda@gmail.com>
2021-07-07 13:55:04 +0900

    add component: ion-property-position.

--- src/components/component/layout-tag-attribute-modal/attribute-ion-label.vue
+++ src/components/component/layout-tag-attribute-modal/attribute-ion-label.vue
@@ -2,6 +2,7 @@
   <div v-if="tagTemp.editTag === 'IonLabel'">
     <ion-property-color v-model="value" />
     <ion-property-mode v-model="value" />
+    <ion-property-position v-model="value" />
     <label for="inputIonLabel" class="small"
     >Label</label
     >
@@ -21,9 +22,10 @@
 <script>
 import IonPropertyColor from "./ion-property/ion-property-color";
 import IonPropertyMode from "./ion-property/ion-property-mode";
+import IonPropertyPosition from "./ion-property/ion-property-position";
 export default {
   name: "attribute-ion-label",
-  components: {IonPropertyMode, IonPropertyColor},
+  components: {IonPropertyPosition, IonPropertyMode, IonPropertyColor},
   props: {
     value: {},
     tagTemp: {},
