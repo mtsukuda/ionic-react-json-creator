be87ad6a6b048ee9524ada8f5d7539c1bc937714
mtsukuda <mats.tsukuda@gmail.com>
2021-07-07 13:54:44 +0900

    create component: ion-property-position.

---
+++ src/components/component/layout-tag-attribute-modal/ion-property/ion-property-position.vue
@@ -0,0 +1,39 @@
+<template>
+<div>
+  <b-form-group
+      label-cols-sm="2"
+      label-cols-lg="2"
+      content-cols-sm
+      content-cols-lg="8"
+      description="The position determines where and how the label behaves inside an item."
+      label="Color"
+      label-for="input-horizontal"
+  >
+    <b-form-select v-model="value.readyProps.position" :options="options" />
+  </b-form-group>
+
+</div>
+</template>
+
+<script>
+export default {
+  name: "ion-property-position",
+  props: {
+    value: {},
+  },
+  data() {
+    return {
+      options: [
+        {value: '', text: 'Please select position.'},
+        {value: "'fixed'", text: 'fixed'},
+        {value: "'floating'", text: 'floating'},
+        {value: "'stacked'", text: 'stacked'},
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
