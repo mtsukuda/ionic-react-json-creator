c92f9c0e9e01689e80b6b0d76de456e56ceba64c
mtsukuda <mats.tsukuda@gmail.com>
2021-07-07 13:49:29 +0900

    create attribute-ion-label.

---
+++ src/components/component/layout-tag-attribute-modal/attribute-ion-label.vue
@@ -0,0 +1,37 @@
+<template>
+  <div v-if="tagTemp.editTag === 'IonLabel'">
+    <ion-property-color v-model="value" />
+    <ion-property-mode v-model="value" />
+    <label for="inputIonLabel" class="small"
+    >Label</label
+    >
+    <div class="col-sm">
+      <input
+          class="form-control"
+          size="sm"
+          id="inputIonLabel"
+          type="text"
+          placeholder="Card Title"
+          v-model="value.content"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import IonPropertyColor from "./ion-property/ion-property-color";
+import IonPropertyMode from "./ion-property/ion-property-mode";
+export default {
+  name: "attribute-ion-label",
+  components: {IonPropertyMode, IonPropertyColor},
+  props: {
+    value: {},
+    tagTemp: {},
+    fetchTemp: {}
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
