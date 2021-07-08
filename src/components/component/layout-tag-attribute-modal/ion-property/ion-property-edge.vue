16f8cf7d5b94110750134dc95bdf7c53fa6f70c2
mtsukuda <mats.tsukuda@gmail.com>
2021-07-09 01:40:51 +0900

    create component: ion-property-edge.

---
+++ src/components/component/layout-tag-attribute-modal/ion-property/ion-property-edge.vue
@@ -0,0 +1,38 @@
+<template>
+<div>
+  <b-form-group
+      label-cols-sm="2"
+      label-cols-lg="2"
+      content-cols-sm
+      content-cols-lg="8"
+      description='If true, the fab will display on the edge of the header if vertical is "top", and on the edge of the footer if it is "bottom". Should be used with a fixed slot.'
+      label="Edge"
+      label-for="input-horizontal"
+  >
+    <b-form-select v-model="value.readyProps.edge" :options="options" />
+  </b-form-group>
+
+</div>
+</template>
+
+<script>
+export default {
+  name: "ion-property-edge",
+  props: {
+    value: {},
+  },
+  data() {
+    return {
+      options: [
+        {value: '', text: 'Please select true/false.'},
+        {value: "{true}", text: 'true'},
+        {value: "{false}", text: 'false'}
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
