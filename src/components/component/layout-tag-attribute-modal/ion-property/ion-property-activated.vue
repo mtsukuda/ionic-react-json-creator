5329a11478b1870668af2689b5a26cb1a3238400
mtsukuda <mats.tsukuda@gmail.com>
2021-07-09 01:37:26 +0900

    create component: ion-property-activated.

---
+++ src/components/component/layout-tag-attribute-modal/ion-property/ion-property-activated.vue
@@ -0,0 +1,38 @@
+<template>
+<div>
+  <b-form-group
+      label-cols-sm="2"
+      label-cols-lg="2"
+      content-cols-sm
+      content-cols-lg="8"
+      description="If true, both the ion-fab-button and all ion-fab-list inside ion-fab will become active. That means ion-fab-button will become a close icon and ion-fab-list will become visible."
+      label="Activated"
+      label-for="input-horizontal"
+  >
+    <b-form-select v-model="value.readyProps.activated" :options="options" />
+  </b-form-group>
+
+</div>
+</template>
+
+<script>
+export default {
+  name: "ion-property-activated",
+  props: {
+    value: {},
+  },
+  data() {
+    return {
+      options: [
+        {value: '', text: 'Please select true/false.'},
+        {value: "{true}", text: 'true'},
+        {value: "{false}", text: 'false'}
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
