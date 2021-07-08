57f33dc2f4f777de5d52f5695286f1d05a40eedf
mtsukuda <mats.tsukuda@gmail.com>
2021-07-09 02:02:15 +0900

    create component: ion-property-router-animation.

---
+++ src/components/component/layout-tag-attribute-modal/ion-property/ion-property-router-animation.vue
@@ -0,0 +1,37 @@
+<template>
+<div>
+  <b-form-group
+      label-cols-sm="2"
+      label-cols-lg="2"
+      content-cols-sm
+      content-cols-lg="8"
+      description="When using a router, it specifies the transition animation when navigating to another page using href."
+      label="Router Animation"
+      label-for="input-vertical"
+  >
+    <b-form-select v-model="value.readyProps.routerAnimation" :options="options" />
+  </b-form-group>
+
+</div>
+</template>
+
+<script>
+export default {
+  name: "ion-property-vertical",
+  props: {
+    value: {},
+  },
+  data() {
+    return {
+      options: [
+        {value: '', text: 'Please select animation type.'},
+        {value: "((baseEl: any, opts?: any) => Animation)", text: 'Animation'},
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
