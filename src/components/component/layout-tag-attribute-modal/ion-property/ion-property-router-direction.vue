59b4a038e0b2fcd08c9b7cc470364b78cf7661f5
mtsukuda <mats.tsukuda@gmail.com>
2021-07-09 02:04:47 +0900

    create component: ion-property-router-direction.

---
+++ src/components/component/layout-tag-attribute-modal/ion-property/ion-property-router-direction.vue
@@ -0,0 +1,39 @@
+<template>
+<div>
+  <b-form-group
+      label-cols-sm="2"
+      label-cols-lg="2"
+      content-cols-sm
+      content-cols-lg="8"
+      description="When using a router, it specifies the transition direction when navigating to another page using href."
+      label="Router Direction"
+      label-for="input-vertical"
+  >
+    <b-form-select v-model="value.readyProps.routerDirection" :options="options" />
+  </b-form-group>
+
+</div>
+</template>
+
+<script>
+export default {
+  name: "ion-property-vertical",
+  props: {
+    value: {},
+  },
+  data() {
+    return {
+      options: [
+        {value: '', text: 'Please select animation type.'},
+        {value: "'back'", text: 'back'},
+        {value: "'forward'", text: 'forward'},
+        {value: "'root'", text: 'root'},
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
