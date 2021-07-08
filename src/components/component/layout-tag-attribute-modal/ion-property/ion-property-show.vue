3160872707b5a22ad455a620563ab89afcffd9c8
mtsukuda <mats.tsukuda@gmail.com>
2021-07-09 02:07:42 +0900

    create component: ion-property-show.

---
+++ src/components/component/layout-tag-attribute-modal/ion-property/ion-property-show.vue
@@ -0,0 +1,38 @@
+<template>
+<div>
+  <b-form-group
+      label-cols-sm="2"
+      label-cols-lg="2"
+      content-cols-sm
+      content-cols-lg="8"
+      description="If true, the fab button will show when in a fab-list."
+      label="Show"
+      label-for="input-show"
+  >
+    <b-form-select v-model="value.readyProps.show" :options="options" />
+  </b-form-group>
+
+</div>
+</template>
+
+<script>
+export default {
+  name: "ion-property-show",
+  props: {
+    value: {},
+  },
+  data() {
+    return {
+      options: [
+        {value: '', text: 'Please select true/false.'},
+        {value: "{true}", text: 'true'},
+        {value: "{false}", text: 'false'}
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
