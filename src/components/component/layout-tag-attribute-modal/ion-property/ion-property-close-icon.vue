ca8f0eb9e59fd7314951b375ef8d2bfb0330c377
mtsukuda <mats.tsukuda@gmail.com>
2021-07-09 01:54:52 +0900

    create component: ion-property-close-icon.

---
+++ src/components/component/layout-tag-attribute-modal/ion-property/ion-property-close-icon.vue
@@ -0,0 +1,37 @@
+<template>
+<div>
+  <b-form-group
+      label-cols-sm="2"
+      label-cols-lg="2"
+      content-cols-sm
+      content-cols-lg="8"
+      description="The color to use from your application's color palette."
+      label="Close Icon"
+      label-for="input-horizontal"
+  >
+    <b-form-select v-model="value.readyProps.closeIcon" :options="options" />
+  </b-form-group>
+
+</div>
+</template>
+
+<script>
+export default {
+  name: "ion-property-close-icon",
+  props: {
+    value: {},
+  },
+  data() {
+    return {
+      options: [
+        {value: '', text: 'Please select close icon.'},
+        {value: "'close'", text: 'close'},
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
