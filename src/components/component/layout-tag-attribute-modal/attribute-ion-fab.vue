343165089e7c03457a569eadeb934652924c4464
mtsukuda <mats.tsukuda@gmail.com>
2021-07-09 01:48:26 +0900

    create component attribute-ion-fa.

---
+++ src/components/component/layout-tag-attribute-modal/attribute-ion-fab.vue
@@ -0,0 +1,28 @@
+<template>
+  <div v-if="tagTemp.editTag === 'IonFab'">
+    <ion-property-activated v-model="value" />
+    <ion-property-edge v-model="value" />
+    <ion-property-horizontal v-model="value" />
+    <ion-property-vertical v-model="value" />
+  </div>
+</template>
+
+<script>
+import IonPropertyActivated from "./ion-property/ion-property-activated";
+import IonPropertyEdge from "./ion-property/ion-property-edge";
+import IonPropertyHorizontal from "./ion-property/ion-property-horizontal";
+import IonPropertyVertical from "./ion-property/ion-property-vertical";
+export default {
+  name: "attribute-ion-fab",
+  components: {IonPropertyVertical, IonPropertyHorizontal, IonPropertyEdge, IonPropertyActivated},
+  props: {
+    value: {},
+    tagTemp: {},
+    fetchTemp: {}
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
