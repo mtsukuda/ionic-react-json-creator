bb094fcd4a6565208a19f1a6de04239e76f022ff
mtsukuda <mats.tsukuda@gmail.com>
2021-07-09 01:44:31 +0900

    create component: ion-property-vertical.

---
+++ src/components/component/layout-tag-attribute-modal/ion-property/ion-property-vertical.vue
@@ -0,0 +1,39 @@
+<template>
+<div>
+  <b-form-group
+      label-cols-sm="2"
+      label-cols-lg="2"
+      content-cols-sm
+      content-cols-lg="8"
+      description="Where to align the fab horizontally in the viewport."
+      label="Vertical"
+      label-for="input-vertical"
+  >
+    <b-form-select v-model="value.readyProps.vertical" :options="options" />
+  </b-form-group>
+
+</div>
+</template>
+
+<script>
+export default {
+  name: "ion-property-vertical",
+  props: {
+    value: {},
+  },
+  data() {
+    return {
+      options: [
+        {value: '', text: 'Please select vertical type.'},
+        {value: "'bottom'", text: 'bottom'},
+        {value: "'center'", text: 'center'},
+        {value: "'top'", text: 'top'},
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
